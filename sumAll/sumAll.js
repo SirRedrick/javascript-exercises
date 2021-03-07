32b6c356dcea75dc891e44366ef243fbdbcc19d4
Mykyta Batrak <fighter662@gmail.com>
2021-03-07 19:35:39 +0200

    completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	if (typeof num1 != 'number' || typeof num2 != 'number' || num1 < 0 || num2 < 0) return 'ERROR';
+	const start = num1 > num2 ? num2 : num1;
+	const end = num1 > num2 ? num1 : num2;
+	let sum = 0;
 
-}
+	for (let i = start; i <= end; i++) {
+		sum += i;
+	}
 
-module.exports = sumAll
+	return sum;
+};
+
+module.exports = sumAll;
