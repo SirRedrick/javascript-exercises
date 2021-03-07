50499377e86584ec83e51f19f362d7033904ebf7
Mykyta Batrak <fighter662@gmail.com>
2021-03-07 22:40:24 +0200

    completed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+	if (number < 0) return 'OOPS';
 
-}
+	const sequence = [1, 1];
+	for (let i = 2; i < number; i++) {
+		sequence[i] = sequence[i - 2] + sequence[i - 1];
+	}
+	return sequence[number - 1];
+};
 
-module.exports = fibonacci
+module.exports = fibonacci;
