a477004dc1726151b890165f795597c0831e9245
Mykyta Batrak <fighter662@gmail.com>
2021-03-07 19:34:08 +0200

    completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+	const arr = [...array];
+	for (let i = 0; i < args.length; i++) {
+		const position = arr.indexOf(args[i]);
+		if (position != -1) arr.splice(position, 1);
+	}
+	return arr;
+};
 
-}
-
-module.exports = removeFromArray
+module.exports = removeFromArray;
