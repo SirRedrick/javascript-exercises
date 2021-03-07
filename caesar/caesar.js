b81da8bac90ed15b30275b6dcfaf5a7659a28e0a
Mykyta Batrak <fighter662@gmail.com>
2021-03-07 22:33:32 +0200

    completed ceasar shift

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,16 @@
-const caesar = function() {
+const cap_low_lim = 65;
+const low_lim = 97;
 
-}
+const caesar = function (string, offset) {
+	return string
+		.replace(/[A-Z]/g, (match) => {
+			const charCode = +match.charCodeAt(0);
+			return String.fromCharCode(cap_low_lim + ((charCode - cap_low_lim + (26 + (offset % 26))) % 26));
+		})
+		.replace(/[a-z]/g, (match) => {
+			const charCode = +match.charCodeAt(0);
+			return String.fromCharCode(low_lim + ((charCode - low_lim + (26 + (offset % 26))) % 26));
+		});
+};
 
-module.exports = caesar
+module.exports = caesar;
