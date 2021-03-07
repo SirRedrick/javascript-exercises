816d823823390e43c4276821048a644235e88150
Mykyta Batrak <fighter662@gmail.com>
2021-03-07 20:02:44 +0200

    completed palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function() {
+const palindromes = function (string) {
+	const formattedString = string
+		.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
+		.replace(/\s/g, '')
+		.toLowerCase();
+	return formattedString === formattedString.split('').reverse().join('');
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
